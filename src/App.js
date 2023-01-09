27e0521f52d5a9e6d210426af118906cd15967c7
E-Holt <emma.d.holt@gmail.com>
2023-01-09 15:46:31 +1000

    routing for add product and cart

--- src/App.js
+++ src/App.js
@@ -68,6 +68,8 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element= {<MainPage />}>
       <Route path="login" element={<Login />} />
+      <Route path="products/add" element={<AddProduct />} />
+      <Route path="cart" element={<Cart />} />
     </Route>
   )
 )
